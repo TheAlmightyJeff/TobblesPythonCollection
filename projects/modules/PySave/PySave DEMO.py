42a32cb119a210aad0efe94b682ee47f87637aee
TheAlmightyJeff <tobiasgbeard@gmail.com>
2025-12-14 16:31:02 +0000

    awdawdawd

---
+++ projects/modules/PySave/PySave DEMO.py
@@ -0,0 +1,10 @@
+import Pysave as ps
+from time import sleep
+
+print("Thank you for using PySave!")
+sleep(0.5)
+print("PySave is a module developed by tobble.")
+sleep(0.7)
+print("It can read and write varibles to a file so they are saved forever! (untill you delete them.)")
+sleep(0.7)
+print("varibles can also be encrypted so no one can see them! (not without a little work of course.)")
--- projects/modules/PySave/PySave.py
+++ projects/modules/PySave/PySave.py
@@ -5,65 +5,77 @@
 
 # VERSION: BETA (things will change)
 
+#-----------------------------------
+#Edit these, if you want
+#-----------------------------------
+
 # Should the data be encrypted?
 # If you already have data it will change to encryption but not back!
-# Its also not millitary grade or anything, so dont store super sensitive data.
+# Its also not millitary grade or anything, so don't store super sensitive data.
 
 ENCRYPTED = True
 
+# use a file path here, leave blank for same folder. make sure you end in a double slash '\\' e.g 'C:/users/tobble/desktop//'
+# make sure the 'r' is present before the string. It should be >>> save_file_location = r"" <<<
+# Changing the file path will not copy the data over!
+
 save_file_location = r""
 
-# use a file path here, leave blank for same folder. make sure you end in a double slash '\\' e.g 'C:/users/guy/desktop//'
-# make sure the 'r' is present before the string.
-# Changing the file path will not copy the data over!
+# should stuff be printed?
 
 print_status_codes = True
 
-# should status codes print?
-
 #--------------------------------------------------------------
 
-from time import sleep as s
 import os
 
 __all__ = ["writeData", "readData", "delData"]
 
 _filePath = "storage.pysave" if save_file_location == "" else f"{save_file_location}storage.pysave"
 
+
 def _Createfile():
-    
     """Attempt to create the storage file. If it already exists, do nothing."""
-    
     try:
-        
         open(_filePath, "x")
         print("Couldn't find storage file... creating file...")
-        
     except FileExistsError:
-        
         pass
 
-cip = lambda text: ''.join(
-    map(
-        lambda char: (
-            lambda ascii_val: chr(33 + ((ascii_val - 33 + 47) % 94)) if 33 <= ascii_val <= 126 else chr(ascii_val)
-        )(
-            (lambda ascii_code: (ascii_code - 65 + 13) % 26 + 65 if 65 <= ascii_code <= 90 else
-                                (ascii_code - 97 + 13) % 26 + 97 if 97 <= ascii_code <= 122 else ascii_code)(
-                (lambda ascii_code_inner: (ascii_code_inner - 65 + 13) % 26 + 65 if 65 <= ascii_code_inner <= 90 else
-                                          (ascii_code_inner - 97 + 13) % 26 + 97 if 97 <= ascii_code_inner <= 122 else ascii_code_inner
-                )(ord(char))
+
+
+cip = lambda t: ''.join(map(
+    lambda c: (
+        lambda x: (
+            lambda y: (
+                lambda z: chr(33 + ((z - 33 + 47) % 94)) if 33 <= z <= 126 else chr(z)
+            )(
+                (lambda a: (a - 65 + 13) % 26 + 65 if 65 <= a <= 90 else
+                           (a - 97 + 13) % 26 + 97 if 97 <= a <= 122 else a
+                )(
+                    (lambda b: (b - 65 + 13) % 26 + 65 if 65 <= b <= 90 else
+                               (b - 97 + 13) % 26 + 97 if 97 <= b <= 122 else b
+                    )(ord(x))
+                )
             )
-        )
-    , text)
-)
+        )(c)
+    )(c)
+, t))
+
+
+def _enc(text):
+    return cip(text) if ENCRYPTED else text
+
+
+def _dec(text):
+    return cip(text) if ENCRYPTED else text
 
 
 def writeData(varName, data):
-    """Write data to the save file (encrypted if enabled)."""
+    """Write encrypted variable name and data to storage."""
 
-    if ENCRYPTED:
-        data = cip(data)
+    key = _enc(varName)
+    value = _enc(data)
 
     try:
         with open(_filePath, "r") as storage:
@@ -71,23 +83,24 @@ def writeData(varName, data):
     except FileNotFoundError:
         storageContent = []
 
-    if varName in storageContent:
-        index = storageContent.index(varName)
+    if key in storageContent:
+        index = storageContent.index(key)
         if index + 1 < len(storageContent):
-            storageContent[index + 1] = data
+            storageContent[index + 1] = value
         else:
-            storageContent.append(data)
+            storageContent.append(value)
     else:
-        storageContent.append(varName)
-        storageContent.append(data)
+        storageContent.append(key)
+        storageContent.append(value)
 
     with open(_filePath, "w") as storage:
-        for line in storageContent:
-            storage.write(line + "\n")
+        storage.write("\n".join(storageContent) + "\n")
 
 
 def readData(varName):
-    """Read data from the save file (decrypt if enabled)."""
+    """Read data using encrypted variable name."""
+
+    key = _enc(varName)
 
     try:
         with open(_filePath, "r") as storage:
@@ -97,22 +110,19 @@ def readData(varName):
             print("Storage file not found.")
         return None
 
-    if varName not in storageContent:
+    if key not in storageContent:
         if print_status_codes:
             print(f"{varName} not found in save file.")
         return None
 
-    index = storageContent.index(varName) + 1
-    data = storageContent[index]
-
-    if ENCRYPTED:
-        data = cip(data)
-
-    return data
+    index = storageContent.index(key) + 1
+    return _dec(storageContent[index])
 
 
 def delData(varName):
-    """Delete the specified variable from storage. CANNOT BE UNDONE."""
+    """Delete encrypted variable name and value. CANNOT BE UNDONE."""
+
+    key = _enc(varName)
 
     try:
         with open(_filePath, "r") as storage:
@@ -122,16 +132,15 @@ def delData(varName):
             print("Storage file not found.")
         return
 
-    if varName in storageContent:
-        index = storageContent.index(varName)
+    if key in storageContent:
+        index = storageContent.index(key)
         storageContent.pop(index)
         if index < len(storageContent):
             storageContent.pop(index)
 
         with open(_filePath, "w") as storage:
-            for line in storageContent:
-                storage.write(line + "\n")
-                
+            storage.write("\n".join(storageContent) + "\n")
+
         if print_status_codes:
             print(f"Deleted '{varName}' from storage.")
     else:
@@ -140,6 +149,3 @@ def delData(varName):
 
 
 _Createfile()
-
-writeData("test", "1234")
-print(readData("test"))
