4ada5d1d8d0b447cbfbe7420857bd34577a3ed2c
Tobble <146878763+TheAlmightyJeff@users.noreply.github.com>
2025-12-16 09:02:11 +0000

    Update PySave.py

--- projects/modules/PySave/PySave.py
+++ projects/modules/PySave/PySave.py
@@ -8,7 +8,7 @@
 #-----------------------------------
 #Edit these, if you want
 #-----------------------------------
-
+5
 # Should the data be encrypted?
 # If you already have data it will change to encryption but not back!
 # Its also not millitary grade or anything, so don't store super sensitive data.
@@ -121,7 +121,7 @@ def readData(varName):
 
 def delData(varName):
     
-    """Delete encrypted variable name and value. CANNOT BE UNDONE."""
+    """Delete variable name and value, even if encrypted. CANNOT BE UNDONE."""
 
     key = _enc(varName)
 
